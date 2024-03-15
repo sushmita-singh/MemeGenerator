f24b623c9a43cfc2f1a66f4d059a673b5e16e83e
sushmita-singh <sushmitas326@gmail.com>
2024-03-15 13:36:24 +0530

    Complete

--- memeGenerator/src/components/Form.jsx
+++ memeGenerator/src/components/Form.jsx
@@ -1,35 +1,56 @@
 import React, { useState } from 'react'
-import memesData from '../../memesData'
 
 
 
 export default function Form() {
     
+    /**
+    useEffect takes a function as its parameter. If that function
+    returns something, it needs to be a cleanup function. Otherwise,
+    it should return nothing. If we make it an async function, it
+    automatically retuns a promise instead of a function or nothing.
+    Therefore, if you want to use async operations inside of useEffect,
+    you need to define the function separately inside of the callback
+    function, as seen below:
+    */
 
-    const memeArray = memesData.data.memes;
-    
-    const [memeImage, setMemeImage] = useState(memeArray[10].url)
-    const [topText, setTopText] = useState()
-    const [bottomText, setBottomText] = useState()
     const [meme, setMeme] = useState({
         topText: "",
         bottomText: "",
         imgSrc: "http://i.imgflip.com/1bij.jpg"
     })
-    const [allMemeImages, setAllMemeImages] = useState(memesData.data.memes)
+
+    const [allMemes, setAllMemes] = useState()
+
+    React.useEffect(() => {
+        async function getMemes() {
+            const res = await fetch("https://api.imgflip.com/get_memes")
+            const data = await res.json()
+            setAllMemes(data.data.memes)
+        }
+        getMemes()
+    }, [])
 
     function loadMeme() {
         let index = Math.floor(Math.random()*100);
-        console.log(index);
         setMeme(prev => {
             return {
                 ...prev,
-                imgSrc: allMemeImages[index].url
+                imgSrc: allMemes[index].url
             }
         })
     }
 
-
+    function handleClick(event) {
+        const {name, value} = event.target
+        console.log(meme)
+        setMeme(prevValue => ({
+            ...prevValue,
+            [name]: value
+        }))
+        document.getElementById('topText').innerHTML = meme.topText
+        document.getElementById('bottomText').innerHTML = meme.bottomText
+    }
 
     return (
         <>
@@ -37,19 +58,31 @@ export default function Form() {
             <div className='form--data'>
                 <div className='top-text-el'>
                     <label htmlFor="top-text">Top text</label>
-                    <input id='top-text' type='text' />
+                    <input 
+                        id='top-text' 
+                        type='text' 
+                        name='topText'
+                        value={meme.topText}
+                        onChange={handleClick}
+                    />
                 </div>
                 <div className='bottom-text-el'>
                     <label htmlFor="bottom-text">Bottom text</label>
-                    <input id='bottom-text' type='text' />
+                    <input 
+                        id='bottom-text' 
+                        type='text' 
+                        name='bottomText'
+                        value={meme.bottomText}
+                        onChange={handleClick}
+                    />
                 </div>
             </div>
             <button className='submit-btn' onClick={loadMeme}>Get a new meme image 🖼</button>
         </div>
         <div className='meme'>
-            <p className='top-text-image'>{topText}</p>
+            <p className='top-text-image' id='topText'>{meme.topText}</p>
             <img src={meme.imgSrc} className='meme-image' alt='meme' />
-            <p className='bottom-text-image'>{bottomText}</p>
+            <p className='bottom-text-image' id='bottomText'>{meme.bottomText}</p>
         </div>
         </>
     )
